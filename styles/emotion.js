33a812d453bc6c43fe3990bbef448af842355cfa
JeongHyeonSeok <hyeonseok45@gmail.com>
2023-01-02 21:29:24 +0900

    save

---
+++ styles/emotion.js
@@ -0,0 +1,248 @@
+import styled from '@emotion/styled'
+
+export const Contain = styled.main`
+    margin: 50px;
+    margin-bottom: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
+
+export const ContainBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    padding: 102px;
+`;
+
+export const Hown = styled.h1`
+    padding-bottom: 80px;
+`;
+
+
+
+export const UserBox = styled.div`
+    display: flex;
+`;
+
+export const IdFlexStyledBox = styled.div`
+    padding: 1px;
+    display: flex;
+    flex-direction: column;
+    margin-right: 18px;
+`
+
+export const PasswordFlexStyledBox = styled.div`
+    padding: 1px;
+    display: flex;
+    flex-direction: column;
+`
+
+export const UserInformationLabel = styled.label`
+    margin-bottom: 16px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+`;
+
+export const UserInformationInput = styled.input`
+    width: 486px;
+    height: 52px;
+    border: 1px solid #BDBDBD;
+    border-radius: 5px;
+`
+
+export const TitleBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+`
+
+export const TitleLabel = styled.label`
+    margin-bottom: 16px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+`
+
+export const TitleInput = styled.input`
+    width: 996px;
+    height: 52px;
+    border: 1px solid #BDBDBD;
+    border-radius: 5px;
+`
+
+export const ContentBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+`
+
+export const ContentLabel = styled.label`
+    margin-bottom: 16px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+`
+
+export const ContentInput = styled.textarea`
+    width: 996px;
+    height: 480px;
+    border: 1px solid #BDBDBD;
+    border-radius: 5px;
+    resize: none;
+`
+
+export const AddressBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 16px;
+`
+
+export const AddressSpan = styled.span`
+    margin-bottom: 16px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+`
+
+export const AddressSection = styled.div`
+    display: flex;
+    margin-bottom: 16px;
+`
+
+export const AddressInput = styled.input`
+    width: 77px;
+    height: 52px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    margin-right: 16px;
+    text-align: center;
+`
+
+export const AddressButton = styled.button`
+    background: black;
+    color: white;
+    width: 124px;
+    height: 52px;
+`
+
+export const AddressConsole = styled.input`
+    width: 996px;
+    height: 52px;
+    margin-bottom: 16px;
+`
+
+export const YoutubeBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 24px;
+`
+
+export const YoutubeLabel = styled.label`
+    margin-bottom: 16px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px; 
+`
+
+export const YoutubeInput = styled.input`
+    width: 996px;
+    height: 52px;
+    border: 1px solid #BDBDBD;
+    border-radius: 5px;
+`
+
+export const PictureBox = styled.div`
+    margin-top: 40px;
+    width: 996px;
+`
+
+export const PictureText = styled.div`
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    margin-bottom: 16px;
+`
+
+export const PictureSection = styled.div`
+    display: flex;
+`
+
+export const PictureContent = styled.div`
+    background: lightgray;
+    width: 78px;
+    height: 78px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-right: 22px;
+`
+
+export const PictureContentSpan = styled.span`
+    color: #4F4F4F;
+`
+
+export const MainSelectorBox = styled.div`
+    width: 996px;
+    margin-top: 40px;
+`
+
+export const MainSelectorText = styled.div`
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    margin-bottom: 16px;
+`
+
+export const MainSelectorSection = styled.div`
+    display: flex;
+`
+
+export const MainSelectorYoutubeBox = styled.div`
+    margin-right: 11px;
+`
+
+export const MainSelectorPictureBox = styled.div`
+    margin-left: 11px;
+`
+
+export const InputRadio = styled.input`
+    width: 12px;
+    height: 12px;
+`
+
+export const RadioLabel = styled.label`
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    padding-left: 10px;
+`
+
+export const ConfirmBox = styled.div`
+    margin-top: 80px;
+`
+
+export const DeleteButton = styled.button`
+    padding: 16px 60px;
+    margin-right: 24px;
+    border: none;
+    background: lightgray;
+    color: #4F4F4F;
+    text-align: center;
+`
+
+export const EmendButton = styled.button`
+    padding: 16px 60px;
+    border: none;
+    background: #FFD600;
+    color: black;
+    text-align: center;
+`
